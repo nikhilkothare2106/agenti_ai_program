c1c0990c7e4776b07910087dc147f94d9905d506
Nikhil Kothare <nikhilkothare12@gmail.com>
2026-09-17 10:33:05 +0530

    azure model added

--- model_config.py
+++ model_config.py
@@ -13,3 +13,20 @@ model = ChatGroq(
     api_key=api_key,
     temperature=0,
 )
+
+
+# import os
+
+# from dotenv import load_dotenv
+
+# from langchain_openai import AzureChatOpenAI
+
+# load_dotenv()
+
+# model = AzureChatOpenAI(
+#     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),   
+#     azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),  
+#     api_version=os.getenv("AZURE_OPENAI_API_VERSION"),    
+#     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+#     temperature=0,
+# )
